78e0393ca53e1112eb97f3bc6e4e62aff9befda6
shin <skmcoco3@gmail.com>
2021-10-28 21:00:17 +0900

    다트게임 점수매기기

---
+++ programmers/DartGame.java
@@ -0,0 +1,53 @@
+package programmers;
+
+public class DartGame {
+
+    public static int solution(String dartResult) {
+        int answer = 0;
+
+        String temp = ""; // 문자열 내 1번의 점수 저장 변수
+        int arr[] = new int[3]; // 3번의 점수를 저장할 배열
+        int idx = 0; // 배열을 참조하기 위한 idx
+
+        for(int i = 0;i<dartResult.length();i++){
+            switch(dartResult.charAt(i)){
+                case '*':
+                    arr[idx-1]*=2;
+                    if(idx >1) arr[idx-2]*=2;
+                    break;
+                case '#':
+                    arr[idx-1]*=-1;
+                    break;
+                case 'S':
+                    arr[idx] = (int)Math.pow(Integer.parseInt(temp),1);
+                    idx++;
+                    temp ="";
+                    break;
+                case 'D':
+                    arr[idx] = (int)Math.pow(Integer.parseInt(temp),2);
+                    idx++;
+                    temp ="";
+                    break;
+                case 'T':
+                    arr[idx] = (int)Math.pow(Integer.parseInt(temp),3);
+                    idx++;
+                    temp ="";
+                    break;
+                default:
+                    temp+=String.valueOf(dartResult.charAt(i));
+                    break;
+            }
+        }
+
+        for(int i =0;i<arr.length;i++){
+            answer+=arr[i];
+        }
+
+        return answer;
+    }   
+
+    public static void main(String[] args) {
+        String aa = "1S2D*3T";
+        solution(aa);
+    }
+}
