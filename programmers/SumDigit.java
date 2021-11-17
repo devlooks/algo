7e30273e09561876029bbccdbae51f19f65a4465
shin <skmcoco3@gmail.com>
2021-11-17 20:11:38 +0900

    약수 더하기

---
+++ programmers/SumDigit.java
@@ -0,0 +1,23 @@
+package programmers;
+
+public class SumDigit {
+
+    public static int solution (int n) {
+        int answer = 0;
+
+        while (n != 0) {
+            answer += n%10;
+            n /= 10;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        int a = 123;
+
+        int b = solution(a);
+        System.out.println(b);
+    }
+}
